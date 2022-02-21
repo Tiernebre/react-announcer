557193d41b7b49ffd7a1c083dd4e98eee108f8aa
Brendan Tierney <tiernebre@gmail.com>
2022-02-20 20:49:43 -0700

    esbuild setup

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,22 @@
-import typescript from '@rollup/plugin-typescript';
+import esbuild from 'rollup-plugin-esbuild'
 
 export default {
   input: 'src/index.ts',
   output: {
     dir: 'dist',
-    format: 'cjs'
+    format: 'es'
   },
-  plugins: [typescript()]
-};
+  plugins: [
+    esbuild({
+      minify: true,
+      target: 'esnext', // default, or 'es20XX', 'esnext'
+      jsx: 'transform', // default, or 'preserve'
+      jsxFactory: 'React.createElement',
+      jsxFragment: 'React.Fragment',
+      // Like @rollup/plugin-replace
+      define: {
+        __VERSION__: '"x.y.z"',
+      }
+    }),
+  ],
+}
