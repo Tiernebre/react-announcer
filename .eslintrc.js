6b02572611bd7bac1b40e394d258995784c84d9c
Brendan Tierney <tiernebre@gmail.com>
2022-02-20 22:02:22 -0700

    react linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,8 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
     "prettier",
   ],
 };
