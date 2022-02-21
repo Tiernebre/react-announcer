af6391e7aafff369e5832f075753694ba6018ccc
Brendan Tierney <tiernebre@gmail.com>
2022-02-20 20:50:01 -0700

    esbuild

--- rollup.config.js
+++ rollup.config.js
@@ -9,14 +9,7 @@ export default {
   plugins: [
     esbuild({
       minify: true,
-      target: 'esnext', // default, or 'es20XX', 'esnext'
-      jsx: 'transform', // default, or 'preserve'
-      jsxFactory: 'React.createElement',
-      jsxFragment: 'React.Fragment',
-      // Like @rollup/plugin-replace
-      define: {
-        __VERSION__: '"x.y.z"',
-      }
+      target: 'esnext'
     }),
   ],
 }
