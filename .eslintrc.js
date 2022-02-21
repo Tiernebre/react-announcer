95559d4dd20e3d379900dca50bd5e0b77391854c
Brendan Tierney <tiernebre@gmail.com>
2022-02-20 22:05:20 -0700

    detect react

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,8 +11,14 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
     "plugin:unicorn/recommended",
     "prettier",
   ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
 };
