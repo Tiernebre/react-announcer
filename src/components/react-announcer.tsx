9bb9e109c434f357050b6ed4cae380f497f599c7
Brendan Tierney <tiernebre@gmail.com>
2022-02-21 21:07:24 -0700

    styling setup

--- src/components/react-announcer.tsx
+++ src/components/react-announcer.tsx
@@ -1,9 +1,23 @@
+import { CSSProperties } from "react";
+
 type ReactAnnouncerProperties = {
   text?: string;
+};
+
+const style: CSSProperties = {
+  position: "absolute",
+  width: "1px",
+  height: "1px",
+  padding: 0,
+  margin: "-1px",
+  overflow: "hidden",
+  clip: "rect(0, 0, 0, 0)",
+  whiteSpace: "nowrap",
+  borderWidth: 0,
 };
 
 export const ReactAnnouncer = ({
   text,
 }: ReactAnnouncerProperties): JSX.Element => {
-  return <div>{text}</div>;
+  return <div style={style}>{text}</div>;
 };
