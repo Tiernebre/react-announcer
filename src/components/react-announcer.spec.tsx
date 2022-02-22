7ea5156cf5ed161ea8b210905d3ae8b5690e098a
Brendan Tierney <tiernebre@gmail.com>
2022-02-21 21:14:52 -0700

    Styling (#12)
    * styling setup
    
    * politeness
    
    * jest config
    
    * politeness tests
    
    * polite by default

--- src/components/react-announcer.spec.tsx
+++ src/components/react-announcer.spec.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import { ReactAnnouncer } from ".";
+import { Politeness } from "../types";
 
 describe("React Announcer", () => {
   it("displays the text content", () => {
@@ -7,4 +8,19 @@ describe("React Announcer", () => {
     render(<ReactAnnouncer text={text} />);
     expect(screen.getByText(text)).toBeInTheDocument();
   });
+
+  it("by default has a polite aria-live if not specified", () => {
+    const text = "Hello there. This is a test.";
+    render(<ReactAnnouncer text={text} />);
+    expect(screen.getByText(text)).toHaveAttribute("aria-live", "polite");
+  });
+
+  it.each<Politeness>(["polite", "off", "assertive"])(
+    "can be set with politeness = %p",
+    (politeness: Politeness) => {
+      const text = "Hello there. This is a test.";
+      render(<ReactAnnouncer text={text} politeness={politeness} />);
+      expect(screen.getByText(text)).toHaveAttribute("aria-live", politeness);
+    }
+  );
 });
--- src/components/react-announcer.tsx
+++ src/components/react-announcer.tsx
@@ -1,9 +1,30 @@
+import { CSSProperties } from "react";
+import { Politeness } from "../types";
+
 type ReactAnnouncerProperties = {
   text?: string;
+  politeness?: Politeness;
+};
+
+const style: CSSProperties = {
+  position: "absolute",
+  width: "1px",
+  height: "1px",
+  padding: 0,
+  margin: "-1px",
+  overflow: "hidden",
+  clip: "rect(0, 0, 0, 0)",
+  whiteSpace: "nowrap",
+  borderWidth: 0,
 };
 
 export const ReactAnnouncer = ({
   text,
+  politeness = "polite",
 }: ReactAnnouncerProperties): JSX.Element => {
-  return <div>{text}</div>;
+  return (
+    <div aria-live={politeness} style={style}>
+      {text}
+    </div>
+  );
 };
